291b624da1e80de81e0ae5c6680d2b4c36f1b83d
sagarsinghthakur002 <sagarsinghthakur002@gmail.com>
2025-01-03 21:12:28 +0545

    feat: animation

--- script.js
+++ script.js
@@ -1,65 +1,13 @@
-gsap.to(".card1" , {
-    scale:0.7,
-    opacity:0,
-    scrollTrigger: {
-        tigger: ".card1",
-        start: "top 15%",
-        end: "bottom 15%",
-        markers: true,
-        scrub: true,
-
-    }
-}) 
-
-gsap.to(".card2" , {
-    scale:0.7,
-    opacity:0,
-    scrollTrigger: {
-        tigger: ".card2",
-        start: "top 15%",
-        end: "bottom 15%",
-        markers: true,
-        scrub: true,
-
-    }
-}) 
-
-gsap.to(".card3" , {
-    scale:0.7,
-    opacity:0,
-    scrollTrigger: {
-        tigger: ".card3",
-        start: "top 15%",
-        end: "bottom 15%",
-        markers: true,
-        scrub: true,
-
-    }
-}) 
-
-gsap.to(".card4" , {
-    scale:0.7,
-    opacity:0,
-    scrollTrigger: {
-        tigger: ".card4",
-        start: "top 15%",
-        end: "bottom 15%",
-        markers: true,
-        scrub: true,
-
-    }
-}) 
-
-gsap.to(".card5" , {
-    scale:0.7,
-    opacity:0,
-    scrollTrigger: {
-        tigger: ".card5",
-        start: "top 15%",
-        end: "bottom 15%",
-        markers: true,
-        scrub: true,
-
-    }
-}) 
-
+document.querySelectorAll(".card").forEach(card => {
+    gsap.to(card, {
+        scale: 0.7,
+        opacity: 0,
+        scrollTrigger: {
+            trigger: card,
+            start: "top 15%",
+            end: "bottom 15%",
+            markers: true,
+            scrub: true
+        }
+    });
+});
